04355b74494fa89c949cc620959d50572d99d6ef
songren <songren@qq.com>
2022-08-01 15:23:47 +0800

    3

---
+++ js/列表.js
@@ -0,0 +1,5 @@
+var swiper = new Swiper(".swiper-container", {
+    // 显示多少个 silide
+    slidesPerView: 6,
+    spaceBetween: 20
+});
---
+++ js/轮播图.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".h", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop : true,
+});
+var swiper1 = new Swiper(".w", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+});
